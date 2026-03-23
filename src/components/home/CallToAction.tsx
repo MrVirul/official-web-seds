6f6ee1e65976652e94f048184150d9d920db114e
Hasitha Erandika <wickramasinghe.erandika@gmail.com>
2026-03-23 06:17:55 +0530

    style: remove black background from CallToAction

--- src/components/home/CallToAction.tsx
+++ src/components/home/CallToAction.tsx
@@ -15,20 +15,13 @@ const CallToAction = () => {
   return (
     <section
       ref={sectionRef}
-      className="relative flex min-h-screen flex-col items-center justify-start overflow-hidden bg-black pb-24 pt-0"
+      className="relative flex min-h-screen flex-col items-center justify-start overflow-hidden bg-transparent pb-24 pt-0"
     >
       {/* Hands image - Full width at top, non-overlapping */}
       <motion.div
         className="relative w-full z-0"
         style={{ y: imageY, opacity }}
       >
-        {/* White-purple glow behind hands
-        <div 
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[60%] h-[40%] rounded-full blur-[120px] pointer-events-none z-[-1]"
-          style={{
-            background: 'radial-gradient(circle, rgba(255,255,255,0.4) 0%, rgba(224,182,228,0.2) 60%, transparent 100%)'
-          }}
-        /> */}
         <img
           src="/cta-hands.png"
           alt="Hands"
@@ -36,13 +29,7 @@ const CallToAction = () => {
           style={{ mixBlendMode: 'screen' }}
           loading="lazy"
         />
-        {/* Subtle fade at the bottom of the image */}
-        <div
-          className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-transparent to-black"
-        />
       </motion.div>
-
-
     </section>
   );
 };
